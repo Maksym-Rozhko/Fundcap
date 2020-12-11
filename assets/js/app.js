6fcd147c2244770b1cc8fbc90cfbbe7fb4a0347e
Maksym <maksymRozhko7@gmail.com>
2020-12-11 10:02:17 +0200

    feat: added new section;
    - fixed burger button (close) ;

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,16 @@
 const overflowEffect = document.querySelector('#body');
 const burgerMenu = document.querySelector('#burgerToogle');
 const navMenu = document.querySelector('.header__nav__inner');
+const burgerLineTop = document.querySelector('.line-top');
+const burgerLineMiddle = document.querySelector('.line-middle');
+const burgerLineBottom = document.querySelector('.line-bottom');
 
 burgerMenu.addEventListener('click', (e) => {
     e.preventDefault();
     navMenu.classList.toggle('header__nav__inner--active');
     overflowEffect.classList.toggle('overflow');
+    burgerMenu.classList.toggle('active');
+    burgerLineTop.classList.toggle('active');
+    burgerLineMiddle.classList.toggle('active');
+    burgerLineBottom.classList.toggle('active');
 });
